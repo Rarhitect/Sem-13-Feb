8b30c2eff9d5da159f8afa277ee8c3c8c5858a3e
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-13 17:12:56 +0300

    Initial Commit

---
+++ Семинар 13 февраля/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Семинар 13 февраля
+//
+//  Created by Alyaev Roman on 13.02.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
